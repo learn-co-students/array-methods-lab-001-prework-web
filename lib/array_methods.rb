b50791e726f7e973ed0fd0076dc4c9ce7d92d005
angyangie <angie.ramirez12@gmail.com>
2016-02-15 19:36:43 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,27 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
-def using_sort(array)
 
+def using_sort(array)
+  array.sort
+#This method takes in an argument of an array and should return the array, sorted in ascending order. Use the #sort method on that array to get this test passing.
 end
 
 def using_reverse(array)
-
+  array.reverse
+#This method takes in an argument of an array and should return the array with the contents in reverse order. Use the #reverse method on the array to the this test passing.
 end
 
 def using_first(array)
-
+    array.first
+#This method takes in an argument of an array and should return the first element of that array. Try to get this test passing without using the [] method. Use the the #first method instead.
 end
 
 def using_last(array)
-
+  array.last
 end
 
 def using_size(array)
-
+  array.size
 end
