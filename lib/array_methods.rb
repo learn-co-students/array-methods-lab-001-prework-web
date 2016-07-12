059a430c0d902a2af2c0bc44909236f34b148b1e
Beatrix H <bealearnscode@gmail.com>
2016-07-12 20:38:23 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,27 @@
 def using_include(array, element)
-
+  if array.include?(element)
+    return true
+  else
+    return false
+  end
 end
 
 def using_sort(array)
-
+  return array.sort
 end
 
 def using_reverse(array)
-
+  return array.reverse
 end
 
 def using_first(array)
-
+  return array.first
 end
 
 def using_last(array)
-
+  return array.last
 end
 
 def using_size(array)
-
+  return array.size
 end
