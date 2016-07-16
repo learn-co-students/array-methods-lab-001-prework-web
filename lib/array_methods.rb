8b1db70dfbbd77f65a1f8383daae8c8c029b7552
arb1nsnmgl <arvinsanmiguel@gmail.com>
2016-07-15 21:48:18 -0400

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,29 @@
 def using_include(array, element)
-
+  arr = array.include?(element)
+  return arr
 end
 
 def using_sort(array)
-
+  arr = array.sort
+  return arr
 end
 
 def using_reverse(array)
-
+  arr = array.reverse
+  return arr
 end
 
 def using_first(array)
-
+  arr = array.first 
+  return arr
 end
 
 def using_last(array)
-
+  arr = array.last 
+  return arr 
 end
 
 def using_size(array)
-
+  arr = array.size
+  return arr
 end
