5a9e5c1319a78238dcdb70a81643622d897622fb
Naveed Shah <naveed@tayybah.com>
2016-07-18 18:16:43 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,23 @@
 def using_include(array, element)
-
+  return array.include?(element)
 end
 
 def using_sort(array)
-
+  return array.sort
 end
 
 def using_reverse(array)
-
+  return array.reverse
 end
 
 def using_first(array)
-
+  return array[0]
 end
 
 def using_last(array)
-
+  return array[-1]
 end
 
 def using_size(array)
-
+  return array.size
 end
